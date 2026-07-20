febfa6b858396c3b0e57bd35b671c2902c381c21
Zeeshan Haider <ZeeshanHaider.1237750@gmail.com>
2026-07-20 21:30:17 +0500

    Finalized

--- script.js
+++ script.js
@@ -1,26 +1,51 @@
-// const oneForm = document.querySelector("#one");
-// const twoForm = document.querySelector("#two");
-// const threeForm = document.querySelector("#three");
-// const fourForm = document.querySelector("#four");
-// const fiveForm = document.querySelector("#five");
-// const sixForm = document.querySelector("#six");
-// const sevenForm = document.querySelector("#seven");
-// const eightForm = document.querySelector("#eight");
-// const nineForm = document.querySelector("#nine");
-// const tenForm = document.querySelector("#ten");
+const submitButton = document.querySelector("#submit");
+const formInput = document.querySelectorAll(
+  "input:not([type='reset']):not([type='submit'])",
+);
+const resetButton = document.querySelector("#reset");
 
-const formInput = document.querySelectorAll("input:not([type='reset'])");
+let isValid = true;
 
-// const form = [];
 
 formInput.forEach((formElement) => {
-    formElement.addEventListener('input', () => {
-            const errorSpan = formElement.nextElementSibling;
-    if(formElement.validity.valueMissing){
-        errorSpan.textContent = "Pls, Input the correct date."
+  formElement.addEventListener("input", () => {
+    const errorSpan = formElement.nextElementSibling;
+    if (formElement.validity.valueMissing) {
+      errorSpan.textContent = "Pls, Input the Data.";
+    } else if (formElement.validity.typeMismatch) {
+      errorSpan.textContent = "Pls, Input the correct Data";
+    } else {
+      errorSpan.textContent = "";
+    }
+  });
+});
+
+submitButton.addEventListener("click", (event) => {
+  formInput.forEach((formElement) => {
+    const errorSpan = formElement.nextElementSibling;
+    if (formElement.validity.typeMismatch) {
+      event.preventDefault();
+      isValid = false;
+      errorSpan.textContent = "Pls, Input the Correct Data";
+      formElement.classList.add("error");
+    } else if (formElement.validity.valueMissing) {
+      event.preventDefault();
+      isValid = false;
+      errorSpan.textContent = "Pls, Input the Data here, It is required.";
+      formElement.classList.add("error");
     }
     else{
         errorSpan.textContent = "";
+        formElement.classList.remove("error");
     }
-    });
+  });
 });
+
+resetButton.addEventListener("click", () => {
+    formInput.forEach((formElement) => {
+        const errorSpan = formElement.nextElementSibling;
+        formElement.classList.remove("error");
+        isValid = true;
+        errorSpan.textContent = "";
+    });
+}); 
